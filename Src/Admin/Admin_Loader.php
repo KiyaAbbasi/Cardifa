fa1b7693aeefab31094cba310c19fe2a06dafc9c
Kiya Abbasi <121122536+KiyaAbbasi@users.noreply.github.com>
2025-04-28 19:40:56 +0330

    Update Admin_Loader.php

--- Src/Admin/Admin_Loader.php
+++ Src/Admin/Admin_Loader.php
@@ -1,32 +1,47 @@
 <?php
 /**
- * File Name: Admin_Loader.php
- * Description: لودر تمام کلاس‌های ادمین کاردیفا
- * Since: 1.0.0
- * Author: Kiya Holding
+ * File Name:    Admin_Loader.php
+ * Description:  لودر تمام کلاس‌های ادمین کاردیفا
+ * Since:        1.0.0
+ * Author:       Kiya Holding
  */
 
-namespace Cardifa\Src\Admin;
+namespace Cardifa\Admin;
 
 defined('ABSPATH') || exit;
 
 class Admin_Loader
 {
+    /**
+     * Admin_Loader constructor.
+     *
+     * @since 1.0.0
+     */
     public function __construct()
     {
         $this->load_dependencies();
         $this->initialize_classes();
     }
 
-    private function load_dependencies()
+    /**
+     * بارگذاری فایل‌های مورد نیاز ادمین
+     *
+     * @since 1.0.0
+     */
+    private function load_dependencies(): void
     {
         require_once CARDIFA_PATH . 'Src/Admin/Class-Admin-Menu.php';
         require_once CARDIFA_PATH . 'Src/Admin/Class-Settings.php';
     }
 
-    private function initialize_classes()
+    /**
+     * نمونه‌سازی کلاس‌های ادمین
+     *
+     * @since 1.0.0
+     */
+    private function initialize_classes(): void
     {
-        new \Cardifa\Src\Admin\Class_Admin_Menu();
-        // Class_Settings چون static هست نیازی به new نداره
+        new Class_Admin_Menu();    // منوی اصلی و اسکریپت/استایل ادمین
+        // Class_Settings چون static هست، نیازی به new نداره
     }
 }
