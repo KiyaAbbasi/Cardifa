9744a2ca2915430dfd21f88937f1200d242d89c0
Kiya Abbasi <121122536+KiyaAbbasi@users.noreply.github.com>
2025-04-28 19:42:49 +0330

    Update HookManager.php

--- Src/Services/HookManager.php
+++ Src/Services/HookManager.php
@@ -9,94 +9,43 @@
  * License:         GPLv3 or later
  * License URI:     https://www.gnu.org/licenses/gpl-3.0.html
  * 
- * @package         Cardifa\Core\Services
+ * @package         Cardifa\Services
  */
 
-namespace Cardifa\Src\Services;
+namespace Cardifa\Services;
 
 defined('ABSPATH') || exit;
 
-/**
- * کلاس مدیریت هوک‌ها
- * این کلاس مسئول مدیریت تمام اکشن‌ها و فیلترهای پلاگین است
- *
- * @since      1.0.0
- * @package    Cardifa\Core\Services
- * @author     Kiya Holding
- */
-class HookManager {
+class HookManager
+{
     /**
-     * @var array لیست اکشن‌های ثبت شده
+     * آرایه‌ی هوک‌ها (actions & filters)
+     *
+     * @var array
      */
     private $actions = [];
 
     /**
-     * @var array لیست فیلترهای ثبت شده
-     */
-    private $filters = [];
-
-    /**
-     * ثبت یک اکشن جدید
+     * ثبت یک اکشن
      *
-     * @param string   $hook          نام هوک
-     * @param callable $callback      تابع callback
-     * @param int      $priority      اولویت
-     * @param int      $accepted_args تعداد آرگومان‌های قابل قبول
+     * @param string   $hook
+     * @param callable $callback
+     * @param int      $priority
+     * @param int      $accepted_args
      * @since 1.0.0
      */
-    public function add_action($hook, $callback, $priority = 10, $accepted_args = 1) {
-        $this->actions = $this->add($this->actions, $hook, $callback, $priority, $accepted_args);
+    public function add_action(string $hook, callable $callback, int $priority = 10, int $accepted_args = 1): void
+    {
+        $this->actions[] = compact('hook', 'callback', 'priority', 'accepted_args');
     }
 
     /**
-     * ثبت یک فیلتر جدید
-     *
-     * @param string   $hook          نام هوک
-     * @param callable $callback      تابع callback
-     * @param int      $priority      اولویت
-     * @param int      $accepted_args تعداد آرگومان‌های قابل قبول
-     * @since 1.0.0
-     */
-    public function add_filter($hook, $callback, $priority = 10, $accepted_args = 1) {
-        $this->filters = $this->add($this->filters, $hook, $callback, $priority, $accepted_args);
-    }
-
-    /**
-     * اضافه کردن هوک به آرایه
-     *
-     * @param array    $hooks         آرایه هوک‌ها
-     * @param string   $hook          نام هوک
-     * @param callable $callback      تابع callback
-     * @param int      $priority      اولویت
-     * @param int      $accepted_args تعداد آرگومان‌های قابل قبول
-     * @return array
-     * @since 1.0.0
-     */
-    private function add($hooks, $hook, $callback, $priority, $accepted_args) {
-        $hooks[] = [
-            'hook'          => $hook,
-            'callback'      => $callback,
-            'priority'      => $priority,
-            'accepted_args' => $accepted_args
-        ];
-        return $hooks;
-    }
-
-    /**
-     * ثبت تمام هوک‌ها
+     * اجرای تمامی اکشن‌ها
      *
      * @since 1.0.0
      */
-    public function run() {
-        foreach ($this->filters as $hook) {
-            add_filter(
-                $hook['hook'],
-                $hook['callback'],
-                $hook['priority'],
-                $hook['accepted_args']
-            );
-        }
-
+    public function run(): void
+    {
         foreach ($this->actions as $hook) {
             add_action(
                 $hook['hook'],
